8c48a8adf23e0bd4104f0385851b30ba3453d1ee
eddy-peng <eddyp2@uw.edu>
2022-09-06 15:46:33 -0700

    eddy's first

--- tunnel/comps/NavBar/NavBar.js
+++ tunnel/comps/NavBar/NavBar.js
@@ -1,15 +1,16 @@
-import styles from './NavBar.module.scss'
+import styles from 'NavBar.module.scss'
+import Link from 'next/link'
+
+// Content inside of a hamburger icon
 const Navbar = () => {
     return (
         <div id="sidenav" className="sidenav d-flex justify-content-between flex-column">
             <div className="d-flex flex-column">
                 <div className="navTabs">
-                    <a href="home.html" id="selected-nav"><sup id="smallText">I </sup>Home</a>
-                    <a href="about.html"><sup id="smallText">II </sup>About</a>
-                    <a target="_blank"
-                        href="https://docs.google.com/forms/d/1W2M3AvMM0hDVRwwXUgqTHfUfXxNW-ao13U9OoGff3BY/edit?usp=sharing"><sup
-                            id="smallText">III </sup>Feedback</a>
-                    <a href="credits.html"><sup id="smallText">IX </sup>Credits</a>
+                    <Link href="/Gallery"><a><sup className={styles.smallText}>I </sup>Home</a></Link>
+                    <Link href="/About"><a><sup className={styles.smallText}>II </sup>About</a></Link>
+                    <Link href="https://docs.google.com/forms/d/1W2M3AvMM0hDVRwwXUgqTHfUfXxNW-ao13U9OoGff3BY/edit?usp=sharing" target="_blank"><a><sup className={styles.smallText}>III </sup>Feedback</a></Link>
+                    <Link href="/Credits"><a><sup className={styles.smallText}>IX </sup>Credits</a></Link>
                 </div>
             </div> 
             <div className="mb-5 linkGroup">
