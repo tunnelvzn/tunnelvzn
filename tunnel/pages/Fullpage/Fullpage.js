1f698fcd8ef7b368daf2fd2b3222eab9b8ed8825
eddy-peng <eddyp2@uw.edu>
2022-10-02 20:44:15 -0700

    edits

--- tunnel/pages/Fullpage/Fullpage.js
+++ tunnel/pages/Fullpage/Fullpage.js
@@ -5,20 +5,11 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import ReactFullpage from '@fullpage/react-fullpage'
 
-// NOTE: if using fullpage extensions/plugins put them here and pass it as props
-// const pluginWrapper = () => {
-//   require('./statics/fullpage.scrollHorizontally.min');
-// };
-
 const Fullpage = () => (
   <ReactFullpage
-    // pluginWrapper = {pluginWrapper}
-
     //fullpage options
     licenseKey = {'YOUR_KEY_HERE'}
     scrollingSpeed = {1000} /* Options here */
-    scrollHorizontally = {true}  /* Because we are using the extension */
-    scrollHorizontallyKey = {'YOUR KEY HERE'}
 
     render={({ state, fullpageApi }) => {
       return (
