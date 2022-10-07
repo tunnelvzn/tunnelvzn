bbf7d68072c07819853a28d63dc6831103e563df
Eddy Peng <eddypeng@Eddys-MacBook-Air.local>
2022-10-07 15:52:44 -0700

    eddy edits 10/7

--- tunnel/pages/Fullpage/Fullpage.js
+++ tunnel/pages/Fullpage/Fullpage.js
@@ -2,31 +2,29 @@
 
 // Install in terminal: npm install @fullpage/react-fullpage
 import React from 'react'
-import ReactDOM from 'react-dom'
-import ReactFullpage from '@fullpage/react-fullpage'
+import styles from './Fullpage.module.scss'
+import Image from 'next/image'
+import NavBar from '../../comps/NavBar';
 
-const Fullpage = () => (
-  <ReactFullpage
-    //fullpage options
-    licenseKey = {'YOUR_KEY_HERE'}
-    scrollingSpeed = {1000} /* Options here */
+const Fullpage = () => {
+  return (
+    <>
+    <NavBar/>
+    <div className={styles.container}>
+      <div>
+        <Image src="/lonelinessImages/sample.png" className={styles.storyImg} height={400} width={600} />
+        <p className={styles.words}>
+          John Ethel once again finds himself staring out at the surroundings from the light rail during his usual commute to the University of Washington. 
+          He felt isolated. <br/><br/>What infuriated him even more is that it wasn't his choice. In the past, John made efforts to initiate conversations. 
+          Yet, despite his commitment, he came off as disingenuine, which turned people away. So, he gave up and embraced the quiet life.
+        </p>
+      </div>
+      <div>Second Page</div>
+      <div>Third Page</div>
+      <div>Fourth Page</div>
+    </div>
+    </>
+  )
+}
 
-    render={({ state, fullpageApi }) => {
-      return (
-        <ReactFullpage.Wrapper>
-          <div className="section">
-            <p>Section 1 (welcome to fullpage.js)</p>
-            <button onClick={() => fullpageApi.moveSectionDown()}>
-              Click me to move down
-            </button>
-          </div>
-          <div className="section">
-            <p>Section 2</p>
-          </div>
-        </ReactFullpage.Wrapper>
-      );
-    }}
-  />
-);
-
-ReactDOM.render(<Fullpage />, document.getElementById('react-root'));
+export default Fullpage;
