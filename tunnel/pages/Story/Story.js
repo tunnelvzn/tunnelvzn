90686cbe2796171c3aaf7db488df6799a93c5141
Eddy Peng <eddypeng@Eddys-MacBook-Air.local>
2022-10-14 12:33:50 -0700

    eddy edits oct 14

--- tunnel/pages/Story/Story.js
+++ tunnel/pages/Story/Story.js
@@ -5,7 +5,7 @@ const Story = ({ content }) => {
 
     let defaultReturn = (
         <div className='d-inline ms-5 me-5 cursor-pointer'>
-            < Link href="/404" >
+            < Link href="/404">
                 <Image className={styles.story} src={`/../../galleryImages/${content.image}`} height={500} width={300} alt={content.name} />
             </Link >
         </div>
