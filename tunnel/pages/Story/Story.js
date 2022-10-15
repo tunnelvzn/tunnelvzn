8db9daf90763714c3d5fd14363a8ada5323a2146
yunze <yunzez@uw.edu>
2022-10-14 18:44:20 -0700

    .

--- tunnel/pages/Story/Story.js
+++ tunnel/pages/Story/Story.js
@@ -1,11 +1,14 @@
 import styles from './Story.module.scss'
 import Link from 'next/link'
 const Story = ({ content }) => {
-
+    let currImage ='comingSoon.svg'
+    if (content.image) {
+        currImage = content.image
+    }
     let defaultReturn = (
         <div className='d-inline ms-5 me-5 cursor-pointer'>
             < Link href="/404">
-                <img className={styles.story} src={`/../../galleryImages/${content.image}`} alt={content.name} />
+                <img className={styles.story} src={`/../../galleryImages/${currImage}`} alt={content.name} />
             </Link >
         </div>
     )
