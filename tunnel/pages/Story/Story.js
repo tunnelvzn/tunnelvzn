d4c57db3f99e25959ccfdbadc5af0c838a96f1d5
yunze <yunzez@uw.edu>
2022-10-14 18:36:21 -0700

    .

--- tunnel/pages/Story/Story.js
+++ tunnel/pages/Story/Story.js
@@ -6,7 +6,7 @@ const Story = ({ content }) => {
     let defaultReturn = (
         <div className='d-inline ms-5 me-5 cursor-pointer'>
             < Link href="/404">
-                <Image className={styles.story} src={`/../../galleryImages/${content.image}`} height={500} width={300} alt={content.name} />
+                <img className={styles.story} src={`/../../galleryImages/${content.image}`} alt={content.name} />
             </Link >
         </div>
     )
