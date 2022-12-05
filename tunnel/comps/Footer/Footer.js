29a0b9c0e55fc7636502ff2e6ed7e13225484193
Eddy Peng <eddypeng@Eddys-MacBook-Air.local>
2022-12-04 17:49:42 -0800

    overtime 12/4

--- tunnel/comps/Footer/Footer.js
+++ tunnel/comps/Footer/Footer.js
@@ -12,7 +12,7 @@ const Footer = () => {
                 
                 <div className={styles.iconContainer}>
                     <Link href="https://www.instagram.com/tunnel_vzn/"><a className={`${styles.footerLink} ${styles.extraPadding}`} target="_blank"><Icon icon="akar-icons:instagram-fill" className={styles.iconStyle}/></a></Link>
-                    <Link href="https://www.linkedin.com/company/tunnel-vzn/"><a className={`${styles.footerLink} ${styles.extraPadding}`} target="_blank"><Icon icon="akar-icons:linkedin-box-fill" className={styles.iconStyle} /></a></Link>
+                    <Link href="https://www.linkedin.com/company/tunnelvzn/"><a className={`${styles.footerLink} ${styles.extraPadding}`} target="_blank"><Icon icon="akar-icons:linkedin-box-fill" className={styles.iconStyle} /></a></Link>
                     <Link href="mailto: tunnelvzn.uw@gmail.com"><a className={styles.footerLink}><Icon icon="clarity:email-line" width="24" height="24" className={styles.iconStyle} /></a></Link>
                     <Link href="https://ko-fi.com/tunnelvzn"><a className={styles.footerLink} target="_blank"><Icon icon="simple-icons:kofi" width="24" height="24" className={styles.iconStyle} /></a></Link>
                 </div>
