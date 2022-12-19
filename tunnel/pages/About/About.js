69f354c66a51f0c9aedc903285e482d5ef057e23
Eddy Peng <eddypeng@Eddys-MacBook-Air.local>
2022-12-18 19:50:37 -0800

    2nd top 3

--- tunnel/pages/About/About.js
+++ tunnel/pages/About/About.js
@@ -1,13 +1,14 @@
 import styles from './About.module.scss'
 import Accordion from 'react-bootstrap/Accordion';
 import Head from 'next/head'
+import Link from 'next/link'
 
 // Fred, do:
 // npm install react-bootstrap bootstrap
 
 const About = () => {
     return (  
-        <div>
+        <div className={styles.aboutBG}>
             <Head>
                 <title>Tunnel_vzn | About</title>
                 <link rel="icon" href="/favicon.png" />
@@ -51,19 +52,19 @@ const About = () => {
                         <Accordion.Item eventKey="2">
                             <Accordion.Header>Can I suggest a story idea?</Accordion.Header>
                             <Accordion.Body>
-                                Yes, you can! We want to hear about all students' experiences. Although we can't guarantee that your exact idea will be an addition by itself, we can assure you that we will adapt your experience into a future story in some capacity.
+                                Yes, you can! We want to hear about all students' experiences. Although we can't guarantee that your exact idea will be an addition by itself, we can assure you that we will adapt your experience into a future story in some capacity. You can leave your ideas <Link href="https://docs.google.com/forms/d/1W2M3AvMM0hDVRwwXUgqTHfUfXxNW-ao13U9OoGff3BY/edit?usp=sharing"><a className={styles.insertLink} target="_blank">here</a></Link>!
                             </Accordion.Body>
                         </Accordion.Item>
                         <Accordion.Item eventKey="3">
                             <Accordion.Header>Is Tunnel_vzn making any profit?</Accordion.Header>
                             <Accordion.Body>
-                                Nope! Tunnel_vzn is a non-profit project whose sole goal is to tell stories. We do have a Ko-fi donation page. No one is required to donate. We just wanted to feel more official since we have no intention of being sponsored by anyone.
+                                Nope! Tunnel_vzn is a non-profit project whose sole goal is to tell stories. We do have a Ko-fi donation page. No one is required to donate, but we would greatly appreciate it! Ultimately, we just wanted to feel more official since we have no plans of being sponsored by anyone.                            
                             </Accordion.Body>
                         </Accordion.Item>
                         <Accordion.Item eventKey="4">
                             <Accordion.Header>Is the Tunnel_vzn team comprised of students?</Accordion.Header>
                             <Accordion.Body>
-                                Yes! All team members are students at the University of Washington interested in storytelling and trying to be relatable. No one here is getting paid. Please help. I need to feed myself.
+                                Yes! All team members are students at the University of Washington who have a passion for storytelling.
                             </Accordion.Body>
                         </Accordion.Item>
                     </Accordion>
