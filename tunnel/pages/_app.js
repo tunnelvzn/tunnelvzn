4d070412e498f084bcebfd8ba1d2c0aea992dd58
Eddy Peng <eddypeng@Eddys-MacBook-Air.local>
2023-01-16 13:23:24 -0800

    nevermind

--- tunnel/pages/_app.js
+++ tunnel/pages/_app.js
@@ -16,10 +16,6 @@ function MyApp({ Component, pageProps }) {
         <meta name="description" content="A student ran organization telling stories inspired by the experiences of University of Washington students." />
         <meta name="keywords" content="storytelling" />
 
-        <meta property="og:title" content="Tunnel_vzn - Storytelling" />
-        <meta property="og:type" content="website" />
-        <meta property="og:image" content="https://drive.google.com/file/d/1USbIDfD-YuXqWgQKf0Tba9cWRG_dOK1E/view?usp=share_link" />
-        <meta property="og:url" content="https://www.tunnelvzn.org/"/>
       </Head>
       <GlobalProvider>
         <Component {...pageProps} />
