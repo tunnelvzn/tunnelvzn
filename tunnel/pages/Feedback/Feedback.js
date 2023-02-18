3aed7d2a9bf74aecfbab13d1d7ad863c7b765a0f
Eddy Peng <eddypeng@Eddys-MacBook-Air.local>
2023-02-17 17:21:51 -0800

    improved form ui

--- tunnel/pages/Feedback/Feedback.js
+++ tunnel/pages/Feedback/Feedback.js
@@ -1,52 +1,60 @@
 import styles from './Feedback.module.scss'
+import Footer from '/comps/Footer';
+import Head from 'next/head'
 
 export const Feedback = () => {
     return (
-        <div className={`${styles.topSpace}`}>
-            <div className='text-center'>
-                <h1>Feedback</h1>
-                <h6>tell us what you think</h6>
-            </div>
-
-            <div className='mt-4'>
-                <label className='mb-1'> your preferred email</label>
-                <input type="email" className={`${styles.input} w-100`}></input>
-            </div>
-
-            <div className='mt-4'>
-                <label> what do you think about the project so far</label>
-
-                <div className='mt-2'>
-                    <input className='me-2' type="radio" id="Bad" name="fav_language" value="Bad"></input>
-                    <label for="Bad">Bad</label></div>
-
-                <div className='mt-2'>
-                    <input className='me-2' type="radio" id="Okay" name="fav_language" value="Okay"></input>
-                    <label for="Okay">Okay</label></div>
-
-                <div className='mt-2'>
-                    <input className='me-2' type="radio" id="Good" name="fav_language" value="Good"></input>
-                    <label for="Good">Good</label></div>
-
-                <div className='mt-2'>
-                    <input className='me-2' type="radio" id="Awesome" name="fav_language" value="Awesome"></input>
-                    <label for="Awesome">Awesome</label>
+        <>
+            <Head>
+                <title>Tunnel_vzn | Feedback</title>
+                <link rel="icon" href="/favicon.png" />
+            </Head>
+            <div className={styles.feedbackContent}>
+                <div className={styles.block}></div>
+                <div className={`${styles.header} text-center`}>
+                    <h1 className={styles.feedbackTitle}>Feedback</h1>
+                    <h6>We value your ideas! Thank you in advance for filling out this form!</h6>
                 </div>
+                <div className='mt-4'>
+                    <label className={`${styles.question} mb-1`}>1. Your preferred email.</label>
+                    <input type="email" placeholder="email@email.com" className={`${styles.input} w-100`}></input>
+                </div>
+                <div className={`${styles.multi} mt-4`}>
+                    <label className={`${styles.question} mb-1`}>2. What do you think about the project so far?</label>
 
+                    <div className='mt-2'>
+                        <input className='me-2' type="radio" id="Bad" name="fav_language" value="Bad"></input>
+                        <label for="Bad">Bad</label></div>
 
-            </div>
-            <div className='mt-4'>
-                <label className='mb-1'> Based on the previous question, Why do you feel this way?</label>
-                <textarea type="text" className={`${styles.input} ${styles.largeInput} w-100`}></textarea >
-            </div>
-            <div className='mt-4'>
-                <label className='mb-1' > Any issues with usability or other problems with the website you want to bring to our attention?</label>
-                <textarea type="text" className={`${styles.input} ${styles.largeInput}  w-100`}></textarea >
-            </div>
-            <div className='text-center mt-3'>
-                <button className={`${styles.button} `}>Submit</button>
-            </div>
-        </div>)
+                    <div className='mt-2'>
+                        <input className='me-2' type="radio" id="Decent" name="fav_language" value="Decent"></input>
+                        <label for="Decent">Decent</label></div>
+
+                    <div className='mt-2'>
+                        <input className='me-2' type="radio" id="Good" name="fav_language" value="Good"></input>
+                        <label for="Good">Good</label></div>
+
+                    <div className='mt-2'>
+                        <input className='me-2' type="radio" id="Awesome" name="fav_language" value="Awesome"></input>
+                        <label for="Awesome">Awesome</label>
+                    </div>
+                </div>
+                <div className='mt-4'>
+                    <label className={`${styles.question} mb-1`}>3. Based on the previous question, explain why.</label>
+                    <textarea type="text" placeholder="I think this because..." className={`${styles.input} ${styles.largeInput} w-100`}></textarea >
+                </div>
+                <div className='mt-4'>
+                    <label className={`${styles.question} mb-1`}>4. Any usability issues or other obstacles you want to bring to our attention?</label>
+                    <textarea type="text" placeholder="Something I want to bring to your attention is..." className={`${styles.input} ${styles.largeInput}  w-100`}></textarea >
+                </div>
+                <div className={`${styles.buttonContainer} text-center mt-3`}>
+                    <button className={`${styles.button}`}>Submit</button>
+                </div>
+                <div className={styles.block}></div>
+            </div>  
+            <Footer/>  
+        </>
+    )
 }
 
 
